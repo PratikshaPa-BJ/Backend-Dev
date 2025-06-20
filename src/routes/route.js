04ea09dc62b2857f4be78a82de1552113973a67e
Pratiksha-pari <parihar.pratiksh59@gmail.com>
2025-06-21 01:57:47 +0530

    Query Path params basics done

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,11 +1,66 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/test-me', function(req,res){
-    res.send("My First Node JS Application");
+router.get("/test-me", function (req, res) {
+  res.send("My First Node JS Application");
+});
+
+router.get("/students", function (req, res) {
+  let students = ["Pratiksha", "Priyanka", "Bipasha", "Atrika"];
+  console.log(req.params);
+
+  res.send(students);
+});
+
+// Path Parameter
+router.get("/student/:name", function (req, res) {
+  console.log("The Path Parameter in the req are: ");
+  let myParams = req.params;
+  console.log(JSON.stringify(myParams));
+  let myName = myParams.name;
+  console.log(myName);
+
+  res.send("Full name is: " + myName);
+});
+
+router.get("/students/:name/:title", function (req, res) {
+  let fullName = JSON.stringify(req.params);
+  console.log("The Path Parameter in the req are: ", fullName);
+  let x = JSON.parse(fullName);
+  console.log(x);
+
+  res.send("Full name is: " + x.name + " " + x.title);
+});
+
+// Query Params
+
+router.get("/employee", function (req, res) {
+  console.log("Demo of Query params: ");
+  console.log(JSON.stringify(req.query));
+  let customerName = req.query.name;
+  let profile = req.query.profile;
+
+  res.send(customerName + " " + profile);
+});
+
+router.get("/fruits", function (req, res) {
+  console.log("Query params in this request are: ", JSON.stringify(req.query));
+  let arr = ["Mango", "Melon", "Apple"];
+  let i = req.query.index;
+  console.log("The element of specific index is: " , arr[i]);
+  
+  res.send(arr[i]);
+});
+
+router.get('/candidates', function(req,res){
+  console.log(JSON.stringify(req.query));
+  let gender = req.query.gender;
+  let state = req.query.state;
+  let district = req.query.district;
+  console.log("State: ", state, ", District: ", district, ", Gender: " , gender);
+  
+  
+  res.send("Done : " +  state + " ,  " +  district + "  , " + gender)
 })
 
-router.get('/test-you', function(req,res){
-    res.send("My Second API ")
-})
- module.exports = router;
+module.exports = router;
