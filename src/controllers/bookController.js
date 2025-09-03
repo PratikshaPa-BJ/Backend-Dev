a13adfe4e9e1151af30ba68caa9c66e24488af60
Pratiksha-pari <parihar.pratiksh59@gmail.com>
2025-09-04 01:23:31 +0530

    Modify

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -22,14 +22,13 @@ const getSpecificAuthorBooks = async function (req, res) {
   let data = await authorModel
     .find({ author_name: "Chetan Bhagat" })
     .select({ _id: 0, author_id: 1 });
-  // console.log(data);
 
   let authorId = data[0].author_id;
   let data1 = await bookModel
     .find({ author_id: authorId })
     .select({ bookName: 1, _id: 0 });
-  let data2 = data1.map((x) => x.bookName);
 
+  let data2 = data1.map((x) => x.bookName);
   res.send({ allBook: data2 });
 };
 
@@ -53,18 +52,15 @@ const getBookBasedonPrice = async function (req, res) {
   let book = await bookModel
     .find({ price: { $gte: 500, $lte: 1000 } })
     .select({ author_id: 1, _id: 0, bookName: 1 });
-  console.log(book);
   // let bookList = book.map((x) => x.bookName);
 
   let allId = book.map((x) => x.author_id);
-  console.log(allId);
   let author = await authorModel.find({ author_id: { $in: allId } });
   // let authorList = author.map((x) => x.author_name);
 
   book.forEach((x) => {
     const author1 = author.find((y) => x.author_id === y.author_id);
     //  console.log(author1);
-
     x.author_id = author1.author_name;
   });
   // change author_id key with authorName
